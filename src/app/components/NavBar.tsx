0d72c3aaac18b19b1cfa22f1f4893bf5abe8f96a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-20 15:18:01 +0530

    dividing components 🚀

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,7 +3,7 @@ export default function NavBar() {
     <nav className="bg-white p-2 flex justify-between">
       <a href="" className="font-bold text-gray-700 text-2xl">
         {" "}
-        OpenTable{" "}
+        TheRestaurant{" "}
       </a>
       <div>
         <div className="flex">
