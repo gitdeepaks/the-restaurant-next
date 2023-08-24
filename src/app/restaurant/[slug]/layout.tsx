3a7f7216b7dee8eb63b86625227e3e88efcff40a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-24 14:07:53 +0530

    new commit 🚀

--- src/app/restaurant/[slug]/layout.tsx
+++ src/app/restaurant/[slug]/layout.tsx
@@ -7,12 +7,14 @@ export const metadata = {
 
 export default function RetaurantLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { slug: string };
 }) {
   return (
     <>
-      <Header />
+      <Header slug={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
         {children}
       </div>
--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -4,20 +4,62 @@ import RestaurantRating from "./components/RestaurantRating";
 import RestaurantDescription from "./components/RestaurantDescription";
 import RestaurantImages from "./components/RestaurantImages";
 import ReviewCard from "./components/ReviewCard";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { time } from "console";
+import { title } from "process";
 
-export default function RestaurantDetailsPage() {
+const prisma = new PrismaClient();
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+interface RestaurantData {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurant = async (slug: string): Promise<RestaurantData> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+
+  return restaurant;
+};
+
+export default async function RestaurantDetailsPage({ params }: Props) {
+  const restaurant = await fetchRestaurant(params.slug);
+  console.log(restaurant);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
+        <RestaurantNavBar slug={restaurant.slug} />
 
-        <RestaurantTitle />
+        <RestaurantTitle title={restaurant.name} />
 
         <RestaurantRating />
 
-        <RestaurantDescription />
+        <RestaurantDescription description={restaurant.description} />
 
-        <RestaurantImages />
+        <RestaurantImages images={restaurant.images} />
 
         <div>
           <h1 className="font-bold text-3xl mt-10 mb-7 borber-b pb-5">
