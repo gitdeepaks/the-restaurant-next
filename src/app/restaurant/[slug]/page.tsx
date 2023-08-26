858334094de4cd4f0224f0e951c649a668e54be2
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-26 20:59:24 +0530

    new commit 🚀

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -49,7 +49,7 @@ export default async function RestaurantDetailsPage({ params }: Props) {
   console.log(restaurant);
   return (
     <>
-      <div className="bg-white w-[70%] rounded p-3 shadow">
+      <div className="bg-slate-50 w-[70%] rounded p-3 shadow">
         <RestaurantNavBar slug={restaurant.slug} />
 
         <RestaurantTitle title={restaurant.name} />
