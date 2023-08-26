f92c82505d71b1eb93b429a52f03effab0f10ea8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-26 16:25:09 +0530

    new commit 🚀

--- src/app/search/components/SideBar.tsx
+++ src/app/search/components/SideBar.tsx
@@ -1,22 +1,25 @@
+import { Location, Region } from "@prisma/client";
 import React from "react";
 
-function SideBar() {
+function SideBar({
+  locations,
+  regions,
+}: {
+  locations: Location[];
+  regions: Region[];
+}) {
   return (
     <div className="w-1/5">
       <div className="border-b pb-4">
-        <h1 className="mb-2">Region</h1>
-        <p className="font-light text-reg">Toronto</p>
-        <p className="font-light text-reg">Ottawa</p>
-        <p className="font-light text-reg">Montreal</p>
-        <p className="font-light text-reg">Hamilton</p>
-        <p className="font-light text-reg">Kingston</p>
-        <p className="font-light text-reg">Niagara</p>
+        <h1 className="mb-2">Location</h1>
+        {locations.map((location: Location) => (
+          <p className="font-light text-reg capitalize">{location.name}</p>
+        ))}
       </div>
       <div className="border-b pb-4 mt-3">
-        <h1 className="mb-2">Cuisine</h1>
-        <p className="font-light text-reg">Mexican</p>
-        <p className="font-light text-reg">Italian</p>
-        <p className="font-light text-reg">Chinese</p>
+        {regions.map((region: Region) => (
+          <p className="font-light text-reg capitalize">{region.name}</p>
+        ))}
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -23,7 +23,6 @@ const fetchRestaurants = (city: string | undefined) => {
     slug: true,
     region: true,
   };
-
   if (!city) {
     return prisma.restaurant.findMany({ select });
   }
@@ -39,18 +38,31 @@ const fetchRestaurants = (city: string | undefined) => {
   });
 };
 
+const fetchLoactions = () => {
+  return prisma.location.findMany();
+};
+
+const fetchRegions = () => {
+  return prisma.region.findMany();
+};
+
 export default async function SearchPage({
   searchParams,
 }: {
   searchParams: { city: string };
 }) {
   const restaurants = await fetchRestaurants(searchParams.city);
+
+  const locations = await fetchLoactions();
+
+  const regions = await fetchRegions();
+
   console.log(restaurants);
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SideBar />
+        <SideBar locations={locations} regions={regions} />
         <div className="w-5/6">
           {restaurants.length ? (
             restaurants.map((restaurant) => (
