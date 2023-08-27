655d4ee564802bf59aeada57e2909fbc4134ef3d
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-27 21:21:17 +0530

    new commit 🚀

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,28 +4,13 @@ export default function Loading() {
   return (
     <main>
       <Header />
-      <div className="h-96 overflow-hidden animate-pulse bg-slate-200">
-        <div className={`bg-center h-full`} />
-      </div>
-      <div className="flex  m-auto w-2/3 justify-between items-start 0 -mt-9">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <nav className="flex text-reg border-b pb-2">
-            <h4 className="mr-7">Overview</h4>
-            <p className="mr-7">Menu</p>
-          </nav>
-
-          <div className="mt-4 border-b pb-6 animate-pulse bg-slate-200 w-[400px] h-16 rounded"></div>
-
-          <div className="flex items-end animate-pulse">
-            <div className="ratings mt-2 flex items-center">
-              <div className="flex items-center bg-slate-200 w-56"></div>
-              <p className="text-reg ml-3"></p>
-            </div>
-            <div>
-              <p className="text-reg mr-1 ml-4"> </p>
-            </div>
-          </div>
-        </div>
+      <div className="py-3 px-36 mt-10 flex flex-wrap       justify-center">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+          <div
+            className="animate-plus bg-slate-200 w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
+            key={num}
+          />
+        ))}
       </div>
     </main>
   );
