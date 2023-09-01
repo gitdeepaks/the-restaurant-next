8d05d38d0c171b3a6f41dad8a752b2ab3a63eda1
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-01 16:27:51 +0530

    new commit 🚀

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import * as jose from "jose";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const headerInstance = headers();
+  const Bearertoken = headerInstance.get("authorization");
+
+  if (!Bearertoken) {
+    return NextResponse.json({
+      errorMessage: "Invalid credentials",
+    });
+  }
+
+  const token = Bearertoken.split(" ")[1];
+
+  if (!token) {
+    return NextResponse.json({
+      errorMessage: "Token not found",
+    });
+  }
+
+  const signature = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  try {
+    jose.jwtVerify(token, signature);
+  } catch (error) {
+    return NextResponse.json({
+      errorMessage: "Invalid Rquest",
+    });
+  }
+
+  const payLoad = jwt.decode(token) as { email: string };
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payLoad.email,
+    },
+
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true,
+      city: true,
+      phone: true,
+    },
+  });
+
+  return NextResponse.json({
+    id: user,
+  });
+}
