02592197bb9723d0724de8e189592b9c2ed35ec7
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-09 18:59:14 +0530

    new commit 🚀

--- src/app/api/restaurant/[slug]/route.ts
+++ src/app/api/restaurant/[slug]/route.ts
@@ -1,32 +1,133 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 import { times } from "@/data";
 
-//http://localhost:3000/api/restaurant/ramakrishna-indian-restaurant-ottawa?partySize=2&bookingTime=18%3A00&bookingDate=2021-10-20
+const prisma = new PrismaClient();
 
-export function GET(
+export async function GET(
   req: NextRequest,
   { params }: { params: { slug: string } }
 ) {
-  const slug = params.slug;
-  const partySize = req.nextUrl.searchParams.get("partySize") as string;
-  const bookingTime = req.nextUrl.searchParams.get("bookingTime") as string;
-  const bookingDate = req.nextUrl.searchParams.get("bookingDate") as string;
+  const { slug } = params;
 
+  // Extract query parameters
+  const partySize = req.nextUrl.searchParams.get("partySize");
+  const bookingTime = req.nextUrl.searchParams.get("bookingTime");
+  const bookingDate = req.nextUrl.searchParams.get("bookingDate");
+
+  // Validate query parameters
   if (!partySize || !bookingTime || !bookingDate) {
     return NextResponse.json(
       {
-        error: `One of the required Fields are missing PartyTime=${partySize}, BookingTime=${bookingTime} and BookingDate=${bookingDate}`,
+        error: `One or more required fields are missing: PartySize=${partySize}, BookingTime=${bookingTime}, BookingDate=${bookingDate}`,
       },
       { status: 400 }
     );
   }
 
-  const searchedTimes = times.find((time) => {
-    return time.time === bookingTime;
+  // Find the matching time
+  const searchedTimes = times.find((t) => {
+    return t.time === bookingTime;
+  });
+
+  if (!searchedTimes) {
+    return NextResponse.json(
+      {
+        error: `No matching times found for BookingTime=${bookingTime}`,
+      },
+      { status: 404 }
+    );
+  }
+
+  // ... your existing code ...
+
+  const rawBookingDate = req.nextUrl.searchParams.get("bookingDate");
+  if (!rawBookingDate) {
+    return NextResponse.json(
+      {
+        error: `Booking date is missing or invalid.`,
+      },
+      { status: 400 }
+    );
+  }
+
+  const bookingDateFormatted = `${rawBookingDate.slice(
+    4,
+    8
+  )}-${rawBookingDate.slice(0, 2)}-${rawBookingDate.slice(2, 4)}`;
+
+  const rawBookingTime = req.nextUrl.searchParams.get("bookingTime");
+  if (!rawBookingTime) {
+    return NextResponse.json(
+      {
+        error: `Booking time is missing or invalid.`,
+      },
+      { status: 400 }
+    );
+  }
+  const bookingTimeFormatted = rawBookingTime.split("T")[1].slice(0, 5);
+
+  // Now use bookingDateFormatted and bookingTimeFormatted in your database query:
+  const gteDate = new Date(`${bookingDateFormatted}T${bookingTimeFormatted}`);
+  const lteDate = new Date(`${bookingDateFormatted}T${bookingTimeFormatted}`); // Adjust this if necessary
+
+  // try {
+  // Fetch bookings from the database
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: gteDate,
+        lte: lteDate,
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  const bookingTableObj: { [key: string]: { [key: number]: true } } = {};
+
+  bookings.forEach((booking) => {
+    bookingTableObj[booking.booking_time.toISOString()] = booking.tables.reduce(
+      (obj, table) => {
+        return {
+          ...obj,
+          [table.table_id]: true,
+        };
+      },
+      {}
+    );
+  });
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+    },
+  });
+
+  const resTable = restaurant?.tables;
+
+  const searchTimesWithTable = searchedTimes.searchTimes.map((time) => {
+    return {
+      date: new Date(`${bookingDateFormatted}T${time}`),
+      time,
+      tables: resTable,
+    };
   });
 
   return NextResponse.json(
-    { searchTimes: searchedTimes?.searchTimes },
+    {
+      searchedTimes,
+      bookings,
+      bookingTableObj,
+      resTable,
+      searchTimesWithTable,
+    },
     { status: 200 }
   );
 }
