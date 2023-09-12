1b18f9229c9be309b53fc5f2c6a9fd590522361f
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-12 20:50:52 +0530

    new commit 🚀

--- src/app/api/restaurant/[slug]/reserve/route.ts
+++ src/app/api/restaurant/[slug]/reserve/route.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const { slug } = params;
+
+  // Extract query parameters
+  const partySize = req.nextUrl.searchParams.get("partySize") as string;
+  const bookingTime = req.nextUrl.searchParams.get("time") as string;
+  const bookingDate = req.nextUrl.searchParams.get("date") as string;
   return NextResponse.json({
-    ok: "deepak",
+    slug,
+    partySize,
+    bookingTime,
+    bookingDate,
   });
 }
