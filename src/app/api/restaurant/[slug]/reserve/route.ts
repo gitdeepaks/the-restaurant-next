9f45acbac494beee638811488eb62e24bf420d99
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-14 17:20:44 +0530

    new commit 🚀

--- src/app/api/restaurant/[slug]/reserve/route.ts
+++ src/app/api/restaurant/[slug]/reserve/route.ts
@@ -1,3 +1,4 @@
+import { findAvailableTables } from "@/utils/findAvailableTables";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
@@ -15,6 +16,17 @@ export async function GET(
 
   const restaurant = await prisma.restaurant.findUnique({
     where: { slug },
+    select: {
+      tables: {
+        select: {
+          id: true,
+          seats: true,
+          restaurant_id: true,
+        },
+      },
+      open_time: true,
+      close_time: true,
+    },
   });
 
   if (!restaurant) {
@@ -42,10 +54,50 @@ export async function GET(
       };
   }
 
-  return NextResponse.json({
+  const transformedRestaurant = {
+    ...restaurant,
+    opening_time: restaurant.open_time,
+    closing_time: restaurant.close_time,
+  };
+
+  const searchTimesWithTable = await findAvailableTables({
     slug,
-    partySize,
-    bookingTime,
     bookingDate,
+    bookingTime,
+    restaurant: transformedRestaurant,
+  });
+
+  if (!searchTimesWithTable) {
+    return NextResponse.json(
+      {
+        error: `No matching times found for BookingTime=${bookingTime}`,
+      },
+      { status: 404 }
+    );
+  }
+
+  const availableTables = searchTimesWithTable?.find((t) => {
+    return (
+      t.date.toISOString() ===
+      new Date(`${bookingDate}T${bookingTime}`).toISOString()
+    );
+  });
+
+  if (!availableTables) {
+    return NextResponse.json(
+      {
+        error: `No table available for BookingTime=${bookingTime}`,
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({
+    // slug,
+    // partySize,
+    // bookingTime,
+    // bookingDate,
+    // searchTimesWithTable,
+    availableTables,
   });
 }
