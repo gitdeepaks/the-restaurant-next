83c5d0f834f3265211063d82ed9d40bc5a10efeb
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-14 15:43:07 +0530

    new commit 🚀

--- src/app/api/restaurant/[slug]/reserve/route.ts
+++ src/app/api/restaurant/[slug]/reserve/route.ts
@@ -1,4 +1,6 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
 export async function GET(
   req: NextRequest,
@@ -10,6 +12,36 @@ export async function GET(
   const partySize = req.nextUrl.searchParams.get("partySize") as string;
   const bookingTime = req.nextUrl.searchParams.get("time") as string;
   const bookingDate = req.nextUrl.searchParams.get("date") as string;
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+  });
+
+  if (!restaurant) {
+    return (
+      NextResponse.json({
+        errorMessage: `Restaurant with slug ${slug} not found`,
+      }),
+      {
+        status: 404,
+      }
+    );
+  }
+
+  if (
+    new Date(`${bookingDate}T${bookingTime}`) <
+      new Date(`${bookingDate}T${restaurant.open_time}`) ||
+    new Date(`${bookingDate}T${bookingTime}`) >
+      new Date(`${bookingDate}T${restaurant.close_time}`)
+  ) {
+    NextResponse.json({
+      errorMessage: `Restaurant is not open at ${bookingTime} on ${bookingDate}`,
+    }),
+      {
+        status: 404,
+      };
+  }
+
   return NextResponse.json({
     slug,
     partySize,
