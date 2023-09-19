9d2193ab0f88ac8511a3758f81148e40c20690ce
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-19 15:03:59 +0530

    new commit 🚀

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -14,7 +14,7 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
-  const res: FormData = request.body;
+  const res: FormData = await request.json();
 
   const errors: string[] = [];
 
